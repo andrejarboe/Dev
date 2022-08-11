5ff85e782321e7de0cf2913e09936e2e88bc94d2
Andre Jarboe <andrejarboe@gmail.com>
2022-08-11 17:35:09 -0400

    changed file to turtleGraphics.py

--- turtleGraphics .py
+++ turtleGraphics .py
@@ -17,3 +17,17 @@ def square(length):
 
 square(120*2)
 square(120/2)
+
+#from turtle import *
+#color('red', 'yellow')
+#begin_fill()
+#while True:
+#    forward(200)
+#    left(170)
+#    if abs(pos()) < 1:
+#        break
+#end_fill()
+#done()
+
+
+
